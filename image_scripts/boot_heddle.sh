447bd478c7741d2835d8bb4db9bc739021314166
David Halls <dahalls@gmail.com>
2014-12-05 22:50:41 +0000

    Fix USB keyboard input

--- image_scripts/boot_heddle.sh
+++ image_scripts/boot_heddle.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 IMG_DIR="$(dirname "$0")/../images"
-kvm -no-reboot -kernel "build/system-image-${1:-x86_64}/bzImage" -hda "$IMG_DIR/heddle.img" -append "root=/dev/hda rw init=/dist/init.sh console=ttyUSB0,9600n8 console=tty0" -usb -usbdevice serial::vc
+kvm -m 2048 -no-reboot -kernel "build/system-image-${1:-x86_64}/bzImage" -hda "$IMG_DIR/heddle.img" -append "root=/dev/hda rw init=/dist/init.sh console=ttyS0,9600n8 console=tty0"
+#kvm -m 2048 -no-reboot -kernel "build/system-image-${1:-x86_64}/bzImage" -hda "$IMG_DIR/heddle.img" -append "root=/dev/hda rw init=/dist/init.sh console=ttyUSB0,9600n8 console=tty0" -usb -usbdevice serial::vc -usbdevice keyboard
