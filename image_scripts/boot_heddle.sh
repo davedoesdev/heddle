cdea9a7876dbc119b298a72312a72207d60b5c8f
David Halls <dahalls@gmail.com>
2014-12-19 22:41:48 +0000

    Generate qcow2 file (much much smaller)

--- image_scripts/boot_heddle.sh
+++ image_scripts/boot_heddle.sh
@@ -1,13 +1,18 @@
 #/bin/bash
+set -e
 IMG_DIR="$(dirname "$0")/../images"
 
 extra_args=
 part_type=gpt
-while getopts m opt
+img_file=heddle.img
+while getopts mq opt
 do
   case $opt in
     m)
       part_type=msdos
+      ;;
+    q)
+      img_file=heddle.qcow2
       ;;
   esac
 done
@@ -16,4 +21,4 @@ if [ $part_type = gpt ]; then
   extra_args+=" -bios /usr/share/ovmf/OVMF.fd"
 fi
 
-kvm -m 2048 -cpu host -smp 2 -no-reboot -hda "$IMG_DIR/heddle.img" $extra_args
+kvm -m 2048 -cpu host -smp 2 -no-reboot -hda "$IMG_DIR/$img_file" $extra_args
---
+++ image_scripts/make_heddle_qcow2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+HERE="$(dirname "$0")"
+qemu-img convert -c -f raw -O qcow2 "$HERE/../images/heddle."{img,qcow2}
