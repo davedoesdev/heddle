ee0e39ccd71a3378b29bad68286cf94e33d3981c
David Halls <dahalls@gmail.com>
2015-01-05 21:20:14 +0000

    uClibc eventfd patch

--- aboriginal_scripts/run_heddle.sh
+++ aboriginal_scripts/run_heddle.sh
@@ -14,7 +14,7 @@ shift $((OPTIND-1))
 
 export HDB="$IMG_DIR/home.img"
 export HDC="$IMG_DIR/run.img"
-export QEMU_EXTRA="-hdd $IMG_DIR/extra.img"
+export QEMU_EXTRA="-hdd $IMG_DIR/extra.img -redir tcp:5900::5900 -cpu host -smp 2"
 export QEMU_MEMORY=2048 
 export KERNEL_EXTRA="heddle_prepare=$prepare"
 cd "build/system-image-${1:-x86_64}"
