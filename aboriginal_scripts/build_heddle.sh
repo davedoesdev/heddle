8bd23aeffb9f1e5ea22a6a30fdbe9a126889df20
David Halls <dahalls@gmail.com>
2015-02-05 07:08:14 +0000

    Use chroot on travis

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 set -e
+
+use_chroot=
+while getopts c opt
+do
+  case $opt in
+    c)
+      use_chroot=1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
 HERE="$(cd "$(dirname "$0")"; echo "$PWD")"
 export HDB="${HEDDLE_EXT_DIR:-"$HERE/.."}/images/home.img"
 export HDC="${HEDDLE_EXT_DIR:-"$HERE/.."}/images/build.img"
 export QEMU_MEMORY=2048
 cd "build/system-image-${1:-x86_64}"
-./dev-environment.sh
+
+if [ -n "$use_chroot" ]; then
+  # travis doesn't have nested virtualization:
+  # https://github.com/travis-ci/travis-ci/issues/1419
+  mkdir -p chroot
+  sudo mount -o loop -t squashfs hda.sqf chroot
+  sudo mount -o loop "$HDB" chroot/home
+  sudo mount -o loop,ro "$HDC" chroot/mnt
+  sudo mount -t proc proc chroot/proc
+  sudo mount -t sysfs sys chroot/sys
+  sudo mount -t devtmpfs dev chroot/dev
+  sudo mount -t tmpfs tmp chroot/tmp
+  sudo chroot chroot /mnt/init
+  sudo umount chroot/{home,mnt,proc,sys,dev,tmp,}
+else
+  ./dev-environment.sh
+fi
