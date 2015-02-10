5858c7fd9ad4c196e5ba81a6293fa969982c78d1
David Halls <dahalls@gmail.com>
2015-02-10 21:22:53 +0000

    Fix snap build error

--- .semaphore.sh
+++ .semaphore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 rm -rf build-aboriginal-travis heddle
 git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
--- .snap-ci.sh
+++ .snap-ci.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 rm -rf build-aboriginal-travis heddle
 git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
 mv heddle/images/*.img images
 rm -rf build-aboriginal-travis heddle
-curl "http://home.earthlink.net/~k_sheff/sw/e2tools/e2tools-0.0.16.tar.gz" | tar xf
+curl "http://home.earthlink.net/~k_sheff/sw/e2tools/e2tools-0.0.16.tar.gz" | tar -zx
 cd e2tools-0.0.16
 ./configure
 make
