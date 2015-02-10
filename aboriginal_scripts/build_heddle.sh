d6bf64efc1384069643d3f64c419f3928535f8d5
David Halls <dahalls@gmail.com>
2015-02-10 07:28:50 +0000

    Always create /dev/ubd{b,c} so packages picks up it's a UML build

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -26,30 +26,21 @@ if [ -n "$uml" ]; then
 mount -t proc proc /proc
 mount -t tmpfs tmp /tmp
 mkdir /tmp/root
-echo +DEV
-ls /dev
-echo -DEV
-mkdir /tmp/wup
-mount -t devtmpfs dev /tmp/wup
-ls /tmp/wup
-echo .DEV
 if [ -b /dev/ubda ]; then
-#delete this stuff
-#make build steps more robust against rerun and clean up
   mount /dev/ubda /tmp/root
   mount -t devtmpfs dev /tmp/root/dev
-  ln -s ubdb /tmp/root/dev/hdb
-  ln -s ubdc /tmp/root/dev/hdc
 else
   mknod /tmp/ubda b 98 0
   mount /tmp/ubda /tmp/root
   mkdir /tmp/dev
-  mknod /tmp/dev/hdb b 98 16
-  mknod /tmp/dev/hdc b 98 32
+  mknod /tmp/dev/ubdb b 98 16
+  mknod /tmp/dev/ubdc b 98 32
   mknod /tmp/dev/ttyS0 c 4 64
   mknod /tmp/dev/null c 1 3
   mount -o bind /tmp/dev /tmp/root/dev
 fi
+ln -s ubdb /tmp/root/dev/hdb
+ln -s ubdc /tmp/root/dev/hdc
 exec /usr/sbin/chroot /tmp/root ash -c 'exec /sbin/init.sh < /dev/ttyS0 > /dev/ttyS0 2>&1'
 EOF
   chmod +x "$ROOT_DIR/init.uml"
