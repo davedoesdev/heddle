6e702a881153f2eb9f89e2bd8990f2bb0204845b
David Halls <dahalls@gmail.com>
2015-02-10 21:26:26 +0000

    Snap-CI needs e2fsprogs-devel

--- .snap-ci.sh
+++ .snap-ci.sh
@@ -5,6 +5,7 @@ git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
 mv heddle/images/*.img images
 rm -rf build-aboriginal-travis heddle
+sudo yum install e2fsprogs-devel
 curl "http://home.earthlink.net/~k_sheff/sw/e2tools/e2tools-0.0.16.tar.gz" | tar -zx
 cd e2tools-0.0.16
 ./configure
