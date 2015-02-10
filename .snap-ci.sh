731514d51c639ee1e70cba44293f4ecb284fbe11
David Halls <dahalls@gmail.com>
2015-02-10 23:15:58 +0000

    Show kernel version on Snap-CI

--- .snap-ci.sh
+++ .snap-ci.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+uname -a
 rm -rf build-aboriginal-travis heddle
 git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
 mv heddle/images/*.img images
 rm -rf build-aboriginal-travis heddle
-sudo yum install e2fsprogs-devel user-mode-linux
+sudo yum install e2fsprogs-devel user-mode-linux slirp
 curl "http://home.earthlink.net/~k_sheff/sw/e2tools/e2tools-0.0.16.tar.gz" | tar -zx
 cd e2tools-0.0.16
 ./configure
