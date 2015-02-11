c68661441b472264b58aa2fe5ed4a4cec4bca2e8
David Halls <dahalls@gmail.com>
2015-02-11 06:59:00 +0000

    Fix slirp download

--- .snap-ci.sh
+++ .snap-ci.sh
@@ -6,7 +6,7 @@ git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
 mv heddle/images/*.img images
 rm -rf build-aboriginal-travis heddle
-sudo yum install e2fsprogs-devel slirp
+sudo yum install e2fsprogs-devel
 curl "http://home.earthlink.net/~k_sheff/sw/e2tools/e2tools-0.0.16.tar.gz" | tar -zx
 cd e2tools-0.0.16
 ./configure
@@ -15,9 +15,9 @@ ln -s e2cp e2mkdir
 ln -s e2cp e2ls
 PATH="$PATH:$PWD"
 cd ..
-curl "http://ftp.debian.org/debian/pool/main/s/slirp/slirp_1.0.17.orig.tar.gz" | tar -x
+curl "http://ftp.debian.org/debian/pool/main/s/slirp/slirp_1.0.17.orig.tar.gz" | tar -zx
 cd slirp-1.0.17
-curl "http://ftp.debian.org/debian/pool/main/s/slirp/slirp_1.0.17-7.debian.tar.gz" | tar -x
+curl "http://ftp.debian.org/debian/pool/main/s/slirp/slirp_1.0.17-7.debian.tar.gz" | tar -zx
 for p in debian/patches/*.patch; do patch -p1 < "$p"; done
 cd src
 ./configure
