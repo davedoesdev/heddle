081a9f0fab00870848582eb483426dacbd842f6c
David Halls <dahalls@gmail.com>
2015-02-11 22:36:25 +0000

    Back to UML on Semaphore

--- .semaphore.sh
+++ .semaphore.sh
@@ -6,8 +6,7 @@ git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 mv heddle/images/*.img images
 rm -rf build-aboriginal-travis heddle
 sudo apt-get update -qq
-sudo apt-get install -y e2tools qemu-kvm
+sudo apt-get install -y e2tools user-mode-linux slirp
 cd aboriginal-1.3.0
 ../image_scripts/make_build_and_home_images.sh
-sed -i 's/-enable-kvm//' build/system-image-x86_64/run-emulator.sh
-../aboriginal_scripts/build_heddle.sh
+../aboriginal_scripts/build_heddle.sh -u
