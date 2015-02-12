cf2dce4ade39bb0ef7827a6bb56f9c994ec777cf
David Halls <dahalls@gmail.com>
2015-02-12 22:29:48 +0000

    Try guestmount

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -52,7 +52,7 @@ EOF
   linux.uml ubd0=hda.sqf "ubd1=$HDB" "ubd2=$HDC" "hostfs=$ROOT_DIR" rootfstype=hostfs init=/init.uml mem="${QEMU_MEMORY}M" con0=fd:3,fd:4 ssl0=fd:0,fd:1 console=ttyS0 "HOST=${1:-x86_64}" eth0=slirp 3>/dev/null 4>&1
 elif [ -n "$chroot" ]; then
   mkdir /tmp/chroot
-  sudo mount -o loop,ro hda.sqf /tmp/chroot
+  sudo guestmount -a hda.sqf -m /dev/sda --ro /tmp/chroot
   sudo mount -t tmpfs tmp /tmp/chroot/tmp
   sudo mount -o loop "$HDB" /tmp/chroot/home
   sudo mount -o loop,ro "$HDC" /tmp/chroot/mnt
