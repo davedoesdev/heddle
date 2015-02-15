77b1585295460ae705629eceb3b6a04026bc0347
David Halls <dahalls@gmail.com>
2015-02-15 21:49:37 +0000

    More root fs perms

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -55,7 +55,9 @@ ln -s ubdb /dev/hdc
 exec /sbin/init.sh < /dev/ttyS0 > /dev/ttyS0 2>&1
 EOF
   chmod +x "$ROOT_DIR/init.uml"
-  sudo chown root:root "$ROOT_DIR/dev"
+  chmod -R a-w "$ROOT_DIR"
+  chmod a+w "$ROOT_DIR/dev"
+  sudo chown -R root:root "$ROOT_DIR"
   exec linux.uml "ubd0=$HDB" "ubd1=$HDC" "hostfs=$ROOT_DIR" rootfstype=hostfs rw init=/init.uml mem="${QEMU_MEMORY}M" con0=fd:3,fd:4 ssl0=fd:0,fd:1 console=ttyS0 "HOST=${1:-x86_64}" eth0=slirp 3>/dev/null 4>&1
 else
   exec ./dev-environment.sh
