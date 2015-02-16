2657e1578426f294c5c3b3242cc8a2854ccad6e4
David Halls <dahalls@gmail.com>
2015-02-16 09:18:15 +0000

    Mounting ro?

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -81,6 +81,7 @@ elif [ -n "$chroot" ]; then
   sudo mount -o bind /sys /tmp/chroot/sys
   sudo mount -o bind /dev /tmp/chroot/dev 
   sudo mount -o remount,rw /tmp/chroot/dev
+  mount
   ls -al /tmp/chroot/dev
   sudo bash -c 'echo foo > /dev/null'
   sudo bash -c 'echo bar > /tmp/chroot/dev/null'
