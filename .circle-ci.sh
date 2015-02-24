fd67a889c3d7188eae8693c8b227fa64924903a4
David Halls <dahalls@gmail.com>
2015-02-24 08:13:01 +0000

    Install qemu

--- .circle-ci.sh
+++ .circle-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt-get update -qq
-sudo apt-get install -y e2tools
+sudo apt-get install -y e2tools qemu-kvm
 rm -rf build-aboriginal-travis heddle
 git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
