8f1ac5f340f09f65071febdb3d94f107d088f6d5
David Halls <dahalls@gmail.com>
2015-02-24 07:22:45 +0000

    Disable kvm

--- .circle-ci.sh
+++ .circle-ci.sh
@@ -10,6 +10,7 @@ git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 mv heddle/images/*.img images
 rm -rf build-aboriginal-travis heddle
 cd aboriginal-1.3.0
+sed -i -e 's/-enable-kvm//' build/system-image-x86_64/run-emulator.sh
 ../image_scripts/make_build_and_home_images.sh
 ../aboriginal_scripts/build_heddle.sh -c
 ../image_scripts/make_run_and_extra_images.sh
