97a6e1471ac049d13dd4345fe99909fb83be3bda
David Halls <dahalls@gmail.com>
2015-02-25 23:53:06 +0000

    Make circle and semaphore scripts almost the same again

--- .semaphore.sh
+++ .semaphore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt-get update -qq
-sudo apt-get install -y e2tools qemu-kvm parted mtools syslinux
+sudo apt-get install -y e2tools qemu-kvm parted mtools syslinux coreutils
 rm -rf aboriginal-1.3.0 build-aboriginal-travis heddle
 git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
@@ -11,6 +11,7 @@ cd aboriginal-1.3.0
 sed -i -e 's/-enable-kvm//' build/system-image-x86_64/run-emulator.sh
 ( while true; do echo keep alive!; sleep 60; done ) &
 build() {
+  sudo ln -sf /bin/true /sbin/udevadm
   sudo service cassandra stop
   sudo service elasticsearch stop
   sudo service mongodb stop
