e7caeaec374a6c24eddf901ff7a35cd8224f3426
David Halls <dahalls@gmail.com>
2015-02-25 09:21:04 +0000

    Reformat build options

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -19,7 +19,7 @@ do
   esac
 done
 shift $((OPTIND-1))
-echo "build options: uml:$uml chroot:$chroot lproot:$lproot" | tee /dev/tty
+echo "build options: uml=$uml chroot=$chroot lproot=$lproot" | tee /dev/tty
 
 HERE="$(cd "$(dirname "$0")"; echo "$PWD")"
 export HDB="${HEDDLE_EXT_DIR:-"$HERE/.."}/images/home.img"
