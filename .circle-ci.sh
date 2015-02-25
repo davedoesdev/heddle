1f28ca21dd3c208ec890f7302bd13e33340b1744
David Halls <dahalls@gmail.com>
2015-02-25 23:11:32 +0000

    Upgrade coreutils on Circle CI to get conv=sparse

--- .circle-ci.sh
+++ .circle-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt-get update -qq
-sudo apt-get install -y e2tools qemu-kvm parted mtools syslinux
+sudo apt-get install -y e2tools qemu-kvm parted mtools syslinux coreutils
 rm -rf aboriginal-1.3.0 build-aboriginal-travis heddle
 git clone "https://github.com/davedoesdev/build-aboriginal-travis.git"
 ( cd build-aboriginal-travis; curl -L "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/$(git tag | tail -n 1)/build-aboriginal-1.3.0-heddle.tar.xz" | tar -C .. -Jx )
