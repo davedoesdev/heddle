1d1864516e25dec946c6d4bf6d6e7201b015c4ab
David Halls <dahalls@gmail.com>
2015-03-29 20:27:27 +0100

    dist now working on ARM

--- aboriginal_scripts/dist_heddle.sh
+++ aboriginal_scripts/dist_heddle.sh
@@ -23,11 +23,13 @@ UPDATE_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen/$ARCH/dist/update"
 export HDB="$IMG_DIR/home.img"
 export HDC="$IMG_DIR/dist.img"
 export QEMU_EXTRA="-hdd $IMG_DIR/heddle.img -net user,hostname=heddle -net nic"
-if [ "$qemu_mode" -eq 0 ]; then
-  QEMU_EXTRA+=" -cpu host -smp 2"
-fi
 export KERNEL_EXTRA="heddle_dist_reuse=$reuse"
-export QEMU_MEMORY=2048
+if [ "$ARCH" = x86_64 ]; then
+  if [ "$qemu_mode" -eq 0 ]; then
+    QEMU_EXTRA+=" -cpu host -smp 2"
+  fi
+  export QEMU_MEMORY=2048
+fi
 cd "build/system-image-$ARCH"
 ./dev-environment.sh 
 e2cp "$HDC:gen"/{initrd.img,install.sqf,run.sqf} "$UPDATE_DIR"
--- image_scripts/make_compressed_images.sh
+++ image_scripts/make_compressed_images.sh
@@ -2,6 +2,6 @@
 set -e
 HERE="$(dirname "$0")"
 ARCH="${1:-x86_64}"
-IMG_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen/$ARCH/images"
-qemu-img convert -c -f raw -O qcow2 "$IMG_DIR/heddle."{img,qcow2}
-qemu-img convert -c -f raw -O qcow2 "$IMG_DIR/build."{img,qcow2}
+GEN_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen"
+qemu-img convert -c -f raw -O qcow2 "$GEN_DIR/$ARCH/images/heddle."{img,qcow2}
+qemu-img convert -c -f raw -O qcow2 "$GEN_DIR/build."{img,qcow2}
