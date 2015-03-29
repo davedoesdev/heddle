dd5e028677d9836023470ef547542a6c34e8a9e0
David Halls <dahalls@gmail.com>
2015-03-29 22:17:10 +0100

    ARM kboot_heddle support. Need to get framebuffer working and a boot loader for boot_heddle.

--- image_scripts/kboot_heddle.sh
+++ image_scripts/kboot_heddle.sh
@@ -14,5 +14,15 @@ shift $((OPTIND-1))
 
 IMG_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen/$ARCH/images"
 UPDATE_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen/$ARCH/dist/update"
-kvm -m 2048 -cpu host -smp 2 -no-reboot -kernel "$UPDATE_DIR/linux" -initrd "$UPDATE_DIR/initrd.img" -hda "$IMG_DIR/heddle.img" -append "console=ttyS0,9600n8 console=tty0 $*" -net user,hostname=heddle -net nic
+
+if [ "$ARCH" = x86_64 ]; then
+  CMD="qemu-system-x86_64 -enable-kvm -m 2048 -cpu host -smp 2"
+  CON=ttyS0
+else
+  CMD="qemu-system-arm -M versatilepb -cpu arm1136-r2"
+  CON=ttyAMA0
+fi
+
+$CMD -no-reboot -kernel "$UPDATE_DIR/linux" -initrd "$UPDATE_DIR/initrd.img" -hda "$IMG_DIR/heddle.img" -append "console=$CON,9600n8 console=tty0 $*" -net user,hostname=heddle -net nic
+
 # vga=0xF07" -usb -usbdevice serial::vc -usbdevice keyboard -usbdevice "disk:$IMG_DIR/heddle.img"
