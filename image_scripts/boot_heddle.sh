275723687f356a439e9df7150887c3303755d7ed
David Halls <dahalls@gmail.com>
2015-04-20 22:03:20 +0100

    Allow image to be specified in boot_heddle.sh

--- image_scripts/boot_heddle.sh
+++ image_scripts/boot_heddle.sh
@@ -4,8 +4,9 @@ HERE="$(dirname "$0")"
 
 part_type=gpt
 img_file=heddle.img
+img_specified=0
 append=
-while getopts mqa: opt
+while getopts mqa:i: opt
 do
   case $opt in
     m)
@@ -17,13 +18,16 @@ do
     a)
       append="$OPTARG"
       ;;
+    i)
+      img_file="$OPTARG"
+      img_specified=1
+      ;;
   esac
 done
 shift $((OPTIND-1))
 
 ARCH="${1:-x86_64}"
 IMG_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen/$ARCH/images"
-UPDATE_DIR="${HEDDLE_EXT_DIR:-"$HERE/.."}/gen/$ARCH/dist/update"
 
 if [ "$ARCH" = x86_64 ]; then
   CMD="qemu-system-x86_64 -enable-kvm -m 2048 -cpu host -smp 2"
@@ -34,4 +38,8 @@ else
   CMD="qemu-system-arm -m 256 -M versatilepb -cpu arm1136-r2 -kernel $IMG_DIR/boot.kbin"
 fi
 
-$CMD -no-reboot -hda "$IMG_DIR/$img_file" -net user,hostname=heddle -net nic
+if [ $img_specified -eq 0 ]; then
+  img_file="$IMG_DIR/$img_file"
+fi
+
+$CMD -no-reboot -hda "$img_file" -net user,hostname=heddle -net nic
