e75b0aca732af8022e07b65ef8d216d5918478be
David Halls <dahalls@gmail.com>
2015-08-26 07:34:13 +0100

    Revert "Semaphore takes too long and build expires"
    This reverts commit 5936ff85c5207e34535c2e204c8eefef7312a7b3.

---
+++ .semaphore-ci/build.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -e
+
+ps auxw
+service --status-all
+free -m
+
+sudo service apache2 stop
+sudo service cassandra stop
+sudo service elasticsearch stop
+sudo service memcached stop
+sudo service mongod stop
+sudo service mysql stop
+sudo service postgresql stop
+sudo service rabbitmq-server stop
+sudo service rethinkdb stop
+sudo service sphinxsearch stop
+
+ps auxw
+service --status-all
+free -m
+
+version="$(git rev-parse --abbrev-ref HEAD)"
+if [ "$version" = master ]; then
+  version="$(git rev-parse HEAD)"
+fi
+echo "version: $version"
+
+cd aboriginal-*
+sed -i -e 's/-enable-kvm//' build/system-image-x86_64/run-emulator.sh
+( while true; do echo keep alive!; sleep 60; done ) &
+
+build() {
+  ../image_scripts/make_build_and_home_images.sh || return 1
+  ../aboriginal_scripts/build_heddle.sh -c
+}
+logf=heddle-$version-log-x86_64.txt
+if ! build >& ../$logf; then
+  tail -n 1000 ../$logf
+  exit 1
+fi
+tail -n 100 ../$logf
+
+xz ../$logf
+
+(
+e2extract() {
+  e2ls -l "$1:$3" | while read -r l; do
+    if [ -n "$l" ]; then
+      f="$(echo "$l" | awk '{print $NF}')"
+      if [ "$f" != lost+found ]; then
+        m="$(echo "$l" | awk '{print substr($2, length($2)-4, 1)}')"
+        if [ "$m" = 4 ]; then
+          mkdir "$2$3/$f"
+          e2extract "$1" "$2" "$3/$f"
+        else
+          e2cp "$1:$3/$f" "$2$3/$f"
+        fi
+        p="$(echo "$l" | awk '{print substr($2, length($2)-2)}')"
+        chmod "$p" "$2$3/$f"
+      fi
+    fi
+  done
+}
+srcp="heddle-$version-src-x86_64"
+srcf="$HOME/$srcp.tar"
+
+cd ..
+git archive -o heddle.tar.gz HEAD
+bsdtar -s "@^@$srcp/@" -cf "$srcf" heddle.tar.gz
+rm -f heddle.tar.gz
+
+tmpd="$(mktemp -d)"
+e2extract gen/build.img "$tmpd"
+cd "$tmpd/download"
+bsdtar -s "@^@$srcp/@" -rf "$srcf" *
+cd ../host
+bsdtar -s "@^@$srcp/@" -rf "$srcf" *
+rm -rf "$tmpd"
+
+cd "$SEMAPHORE_CACHE_DIR"
+bsdtar -s "@^@$srcp/@" -rf "$srcf" aboriginal-*.tar.gz
+)
+
+prepare_and_dist() {
+  echo "type: $1"
+  prefix="heddle-$version-$1-x86_64"
+  rm -f ../gen/x86_64/images/{extra,heddle}.img
+  ../image_scripts/make_run_and_extra_images.sh $2   || return 1
+  ../aboriginal_scripts/run_heddle.sh -p -q          || return 1
+  ../image_scripts/make_dist_and_heddle_images.sh -l || return 1
+  ../aboriginal_scripts/dist_heddle.sh -q -r         || return 1
+  bsdtar -C .. -s "/^\./$prefix/" -JLcf "$HOME/$prefix.tar.xz" ./gen/x86_64/dist
+}
+prepare_and_dist gpt-btrfs
+prepare_and_dist gpt-ext4 -e
+prepare_and_dist mbr-btrfs -m
+prepare_and_dist mbr-ext4 '-m -e'
+
+ls "$HOME"
