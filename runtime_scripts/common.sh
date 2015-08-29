7c1a7107ed99d45de3eb0af64e25ce034dab127c
David Halls <dahalls@gmail.com>
2015-08-29 21:00:08 +0100

    Revert "Try changing PATH order"
    This reverts commit 95f5f5f5adcba06f88a1af8d9fe59a673d884240.

--- runtime_scripts/common.sh
+++ runtime_scripts/common.sh
@@ -9,7 +9,7 @@ CHROOT_DIR="$HOME/chroot"
 "$HERE/make_chroot.sh" "$CHROOT_DIR"
 
 export INSTALL_DIR="$HOME/install"
-export PATH="/usr/bin:$(echo $PATH | sed 's/\/usr\/distcc://'):$INSTALL_DIR/bin:$INSTALL_DIR/sbin"
+export PATH="$INSTALL_DIR/bin:$INSTALL_DIR/sbin:/usr/bin:$(echo $PATH | sed 's/\/usr\/distcc://')"
 export CPPFLAGS="-I$INSTALL_DIR/include"
 export LDFLAGS="-L$INSTALL_DIR/lib"
 export LD_LIBRARY_PATH="$INSTALL_DIR/lib"
