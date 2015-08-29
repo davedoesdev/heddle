307fe1075c3933efecdac07e8ad7a207a803b78c
David Halls <dahalls@gmail.com>
2015-08-29 08:11:15 +0100

    Try putting root into tmpfs

--- aboriginal_scripts/build_heddle.sh
+++ aboriginal_scripts/build_heddle.sh
@@ -130,20 +130,27 @@ ln -s hdc /tmp/dev/ubdc
 
 mkdir /tmp/root
 mount /tmp/dev/hda /tmp/root
-mount -o bind /tmp/dev /tmp/root/dev
-mount -t proc proc /tmp/root/proc
-mount -t sysfs sys /tmp/root/sys
 
-mount /tmp/dev/hdb /tmp/root/home
-mount -o ro /tmp/dev/hdc /tmp/root/mnt
+mkdir /tmp/chroot
+mount -t tmpfs -o size=1024M tmp /tmp/chroot
+cp -a /tmp/root /tmp/chroot
+
+mount -o bind /tmp/dev /tmp/chroot/dev
+mount -t proc proc /tmp/chroot/proc
+mount -t sysfs sys /tmp/chroot/sys
+
+mount /tmp/dev/hdb /tmp/chroot/home
+mount -o ro /tmp/dev/hdc /tmp/chroot/mnt
 
 export HOME=/home
 export PATH
 
-exec /usr/sbin/chroot /tmp/root /mnt/init < /tmp/dev/ttyS0 > /tmp/dev/ttyS0 2>&1
+exec /usr/sbin/chroot /tmp/chroot /mnt/init < /tmp/dev/ttyS0 > /tmp/dev/ttyS0 2>&1
 EOF
   chmod +x "$ROOT_DIR/init.uml"
-  exec linux.uml ubd0=root.sqf "ubd1=$HDB" "ubd2=$HDC" "hostfs=$ROOT_DIR" rootfstype=hostfs init=/init.uml mem="${BUILD_MEM}M" con0=fd:3,fd:4 ssl0=fd:0,fd:1 console=ttyS0 "heddle_arch=$ARCH" eth0=slirp 3>/dev/null 4>&1
+# can we create a tmpfs inside uml and cp -a /dev/hda to it?
+# then use that as the chroot
+  exec linux.uml ubd0=root.sqf "ubd1=$HDB" "ubd2=$HDC" "hostfs=$ROOT_DIR" rootfstype=hostfs init=/init.uml mem="$((BUILD_MEM + 1024))M" con0=fd:3,fd:4 ssl0=fd:0,fd:1 console=ttyS0 "heddle_arch=$ARCH" eth0=slirp 3>/dev/null 4>&1
 else
   echo "qemu/kvm build" | tee /dev/tty
   if [ "$ARCH" = x86_64 ]; then
