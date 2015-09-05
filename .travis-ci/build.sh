2d17c107a3d218c1ab78e672f8ccfd16b359ed6e
David Halls <dahalls@gmail.com>
2015-09-05 10:07:05 +0100

    Use bsdtar, hopefully quicker for sparse files

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -14,15 +14,14 @@ build() {
   ../image_scripts/make_build_and_home_images.sh || return 1
   ../aboriginal_scripts/build_heddle.sh -c
 }
-logf=heddle-$version-log-x86_64.txt
-if ! build >& ../$logf; then
-  tail -n 1000 ../$logf
+logf="heddle-$version-log-x86_64.txt"
+if ! build >& "../$logf"; then
+  tail -n 1000 "../$logf"
   exit 1
 fi
-tail -n 100 ../$logf
+tail -n 100 "../$logf"
 rm -rf /tmp/chroot/home/source
 sync
-xz ../$logf ../gen/x86_64/images/home.img
-mv ../gen/x86_64/images/home.img.xz ../heddle-$version-home-x86_64.img.xz
-ls -lh ..
-
+cd ..
+bsdtar -Jcf "heddle-$version-home-x86_64.tar.xz" gen/x86_64/images/home.img "$logf"
+ls -lh
