76a2ca9d07b3777751a0628063826ad91843cf42
David Halls <dahalls@gmail.com>
2015-09-05 07:54:40 +0100

    Does sudo: required on Travis support loop mounts?

---
+++ .travis-ci/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+version="$(git rev-parse --abbrev-ref HEAD)"
+if [ "$version" = master ]; then
+  version="$(git rev-parse HEAD)"
+fi
+echo "version: $version"
+
+sudo chmod a+rw /dev/kvm
+
+cd aboriginal-*
+( while true; do echo keep alive!; sleep 60; done ) &
+
+build() {
+  ../image_scripts/make_build_and_home_images.sh || return 1
+  ../aboriginal_scripts/build_heddle.sh -c
+}
+logf=heddle-$version-log-x86_64.txt
+if ! build >& ../$logf; then
+  tail -n 1000 ../$logf
+  exit 1
+fi
+tail -n 100 ../$logf
+xz ../$logf
+
