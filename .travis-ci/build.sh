7153650199e1f24de4e99d82d2cbc8a868bdfd4d
David Halls <dahalls@gmail.com>
2015-09-05 08:56:45 +0100

    Fix version test

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -2,8 +2,7 @@
 set -e
 
 version="$(git rev-parse --abbrev-ref HEAD)"
-echo $version
-if [ "$version" = master ]; then
+if [ "$version" = master -o "$version" = HEAD ]; then
   version="$(git rev-parse HEAD)"
 fi
 echo "version: $version"
