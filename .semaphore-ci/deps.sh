4a0c8c68743793d6009c4207472a7bc4379d6989
David Halls <dahalls@gmail.com>
2015-09-09 23:01:55 +0100

    Revert "Show Node version on Semaphore"
    This reverts commit 58eab1b3853290fe19275c411b11d349b6ba6722.

--- .semaphore-ci/deps.sh
+++ .semaphore-ci/deps.sh
@@ -11,7 +11,7 @@ sudo apt-get install -y e2tools qemu-kvm parted mtools syslinux syslinux-common 
 
 sudo rm -rf /home/runner/{.npm,.phpbrew,.phpunit,.kerl,.kiex,.lein,.nvm,.npm,.phpbrew,.rbenv}
 
-node --version
+nvm install 0.12
 
 service --status-all
 free -m
