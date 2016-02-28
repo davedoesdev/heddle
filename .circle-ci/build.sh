1689e0482c9e5ff6355e9ab0a4990b7f113d1702
David Halls <dahalls@gmail.com>
2016-02-28 21:01:45 +0000

    CircleCI seems to support chroot now - see if it can build the lot

--- .circle-ci/build.sh
+++ .circle-ci/build.sh
@@ -7,15 +7,50 @@ if [ "$version" = master -o "$version" = HEAD ]; then
 fi
 echo "version: $version"
 
-sudo ln -sf /bin/true /sbin/udevadm
-
 cd aboriginal-*
 sed -i -e 's/-enable-kvm//' build/system-image-x86_64/run-emulator.sh
 ( while true; do echo keep alive!; sleep 60; done ) &
 
-../image_scripts/make_build_and_home_images.sh
+build() {
+  ../image_scripts/make_build_and_home_images.sh || return 1
+  ../aboriginal_scripts/build_heddle.sh -c
+}
+logf="../heddle-$version-log-x86_64.txt"
+if ! build >& "$logf"; then
+  tail -n 1000 "$logf"
+  exit 1
+fi
+tail -n 100 "$logf"
+sudo rm -rf /tmp/chroot/home/source
+df -h
+mkdir /tmp/home
+# mount home without recursive bind to get rid of its chroot bind mounts
+sudo mount -o bind /tmp/chroot/home /tmp/home
+homef="../heddle-$version-home-x86_64.tar.gz"
+sudo tar --owner root --group root -zcf "$homef" -C /tmp home
+ls -lh "$homef"
 
-(
+sudo mv "$logf" /
+sudo xz "/$logf"
+
+e2cp -P 400 -O 0 -G 0 "$homef" ../gen/x86_64/images/home.img:home.tar.gz
+rm -f "$homef"
+
+prepare_and_dist() {
+  echo "type: $1"
+  prefix="heddle-$version-$1-x86_64"
+  rm -f ../gen/x86_64/images/{extra,heddle}.img
+  ../image_scripts/make_run_and_extra_images.sh $2   || return 1
+  ../aboriginal_scripts/run_heddle.sh -p -q          || return 1
+  ../image_scripts/make_dist_and_heddle_images.sh -l || return 1
+  ../aboriginal_scripts/dist_heddle.sh -q -r         || return 1
+  sudo bsdtar -C .. -s "/^\./$prefix/" -JLcf "/$prefix.tar.xz" ./gen/x86_64/dist
+}
+prepare_and_dist gpt-btrfs
+prepare_and_dist gpt-ext4 -e
+prepare_and_dist mbr-btrfs -m
+prepare_and_dist mbr-ext4 '-m -e'
+
 e2extract() {
   e2ls -l "$1:$3" | while read -r l; do
     if [ -n "$l" ]; then
@@ -52,78 +87,5 @@ sudo bsdtar -s "@^@$srcp/@" -rf "$srcf" *
 cd ../host
 sudo bsdtar -s "@^@$srcp/@" -rf "$srcf" *
 rm -rf "$tmpd"
-)
-
-hmac() {
-  SECRET="$1" node 3<&0 << 'EOF'
-var hmac = require('crypto').createHmac('sha256', process.env.SECRET);
-require('fs').createReadStream(null, {fd: 3}).pipe(hmac);
-var t = new require('stream').Transform();
-t._transform = function (data, encoding, callback)
-{
-    this.push(data.toString('hex'));
-    callback();
-};
-hmac.pipe(t);
-t.pipe(process.stdout);
-EOF
-}
-
-txf() {
-  URL="$1" node << 'EOF'
-require('http').request(process.env.URL, function (res)
-{
-    if (res.statusCode === 200)
-    {
-        res.pipe(process.stdout);
-    }
-    else
-    {
-        console.error('error', res.statusCode);
-        process.exitCode = 1;
-        res.pipe(process.stderr);
-    }
-}).end();
-EOF
-}
-
-txf_url() {
-  echo "http://txf-davedoesdev.rhcloud.com/default/$(echo -n "$1" | hmac "$DEFAULT_RECEIVER_SECRET")/$1"
-}
-
-homef="../heddle-$version-home-x86_64.tar.gz"
-
-while ! txf "$(txf_url "heddle-$version")" > "$homef"; do sleep 1; done
-while ! mac="$(txf "$(txf_url "heddle-$version.mac")")"; do sleep 1; done
-
-ls -lh "$homef"
-calc_mac="$(hmac "$INTEGRITY_SECRET" < "$homef")"
-if [ "$mac" != "$calc_mac" ]; then
-  echo "hmac mismatch" 1>&2
-  exit 1
-fi
-
-logf="heddle-$version-log-x86_64.txt"
-# If $home isn't for this version, it won't contain $logf
-sudo tar -zxf "$homef" -C / "$logf"
-sudo xz "/$logf"
-
-e2cp -P 400 -O 0 -G 0 "$homef" ../gen/x86_64/images/home.img:home.tar.gz
-rm -f "$homef"
-
-prepare_and_dist() {
-  echo "type: $1"
-  prefix="heddle-$version-$1-x86_64"
-  rm -f ../gen/x86_64/images/{extra,heddle}.img
-  ../image_scripts/make_run_and_extra_images.sh $2   || return 1
-  ../aboriginal_scripts/run_heddle.sh -p -q          || return 1
-  ../image_scripts/make_dist_and_heddle_images.sh -l || return 1
-  ../aboriginal_scripts/dist_heddle.sh -q -r         || return 1
-  sudo bsdtar -C .. -s "/^\./$prefix/" -JLcf "/$prefix.tar.xz" ./gen/x86_64/dist
-}
-prepare_and_dist gpt-btrfs
-prepare_and_dist gpt-ext4 -e
-prepare_and_dist mbr-btrfs -m
-prepare_and_dist mbr-ext4 '-m -e'
 
 ls -lh /heddle*
