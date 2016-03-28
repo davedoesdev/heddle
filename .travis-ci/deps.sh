f00938d927dad108397faf7ed31a013cdcbbb665
David Halls <dahalls@gmail.com>
2016-03-28 20:49:41 +0100

    Don't need aboriginal.tar.gz on travis

--- .travis-ci/deps.sh
+++ .travis-ci/deps.sh
@@ -5,21 +5,11 @@ mkdir -p downloads
 echo +downloads:
 ls downloads
 
-ver_abo=1.4.5
 ver_bat=6.0.2
-
-abo_base="downloads/aboriginal-$ver_abo"
-abo_seal="$abo_base.seal"
-abo_file="$abo_base.tar.gz"
 
 bat_base="downloads/build-aboriginal-$ver_abo-heddle-x86_64-$ver_bat"
 bat_seal="$bat_base.seal"
 bat_file="$bat_base.tar.xz"
-
-if [ ! -f "$abo_seal" ]; then
-  curl -L -o "$abo_file" "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/v$ver_bat/aboriginal-$ver_abo.tar.gz"
-  touch "$abo_seal"
-fi
 
 if [ ! -f "$bat_seal" ]; then
   curl -L -o "$bat_file" "https://github.com/davedoesdev/build-aboriginal-travis/releases/download/v$ver_bat/build-aboriginal-$ver_abo-heddle-x86_64.tar.xz"
@@ -31,7 +21,7 @@ bsdtar -Jxf "$bat_file"
 mv heddle/gen/build.img gen
 rm -rf heddle
 
-find downloads -mindepth 1 -not -path "$abo_base.*" -not -path "$bat_base.*" -exec rm -v {} \;
+find downloads -mindepth 1 -not -path "$bat_base.*" -exec rm -v {} \;
 echo -downloads:
 ls downloads
 
